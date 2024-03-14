d349baf952fee33c68adbfa97a909101b153d49d
Melissa J. Chin <melissa1jchin@gmail.com>
2024-03-14 12:22:51 -0500

    created seed file

---
+++ server/db/seed.js
@@ -0,0 +1,128 @@
+const client = require("./client");
+const { websiteContent, users, userWebsites } = require("./seedData");
+
+async function dropTables() {
+  try {
+    console.log("Dropping All Tables...");
+    await client.query(`
+      DROP TABLE IF EXISTS website_content CASCADE;
+      DROP TABLE IF EXISTS users CASCADE;
+      DROP TABLE IF EXISTS user_websites CASCADE;
+    `);
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function createTables() {
+  try {
+    console.log("Building All Tables...");
+    await client.query(`
+        CREATE TABLE website_content (
+            website_id SERIAL PRIMARY KEY,
+            main_heading TEXT,
+            subheading TEXT,
+            main_photo TEXT,
+            p1_heading TEXT,
+            p1_body TEXT,
+            mid_photo1 TEXT,
+            mid_photo2 TEXT,
+            p2_heading TEXT,
+            p2_body TEXT,
+            footer_photo TEXT,
+        );
+        CREATE TABLE users (
+            user_id SERIAL PRIMARY KEY,
+            first_name TEXT NOT NULL,
+            username TEXT UNIQUE NOT NULL,
+            password TEXT NOT NULL, 
+        );
+        CREATE TABLE user_websites (
+            user_website_id SERIAL PRIMARY KEY, 
+            user_id INTEGER REFERENCES users(user_id),
+            website_id INTEGER REFERENCES website_content(website_id),
+        );
+        `);
+  } catch (error) {
+    console.log("error creating tables");
+    throw error;
+  }
+}
+
+const createInitialWebsiteContent = async () => {
+  try {
+    for (const website of websiteContent) {
+      await client.query(
+        `
+            INSERT INTO website_content(main_heading, subheading, main_photo, p1_heading, p1_body, mid_photo1, mid_photo2, p2_heading, p2_body, footer_photo)
+            VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);
+        `,
+        [
+          website.main_heading,
+          website.subheading,
+          website.main_photo,
+          website.p1_heading,
+          website.p1_body,
+          website.mid_photo1,
+          website.mid_photo2,
+          website.p2_heading,
+          website.p2_body,
+          website.footer_photo,
+        ]
+      );
+    }
+    console.log("Created Website Content table!");
+  } catch (error) {
+    throw error;
+  }
+};
+
+const createInitialUsers = async () => {
+  try {
+    for (const user of users) {
+      await client.query(
+        `
+            INSERT INTO users(first_name, username, password)
+            VALUES($1, $2, $3);
+        `,
+        [user.first_name, user.username, user.password]
+      );
+    }
+    console.log("Created Users table!");
+  } catch (error) {
+    throw error;
+  }
+};
+
+const createInitialUserWebsites = async () => {
+  try {
+    for (const userWebsite of userWebsites) {
+      await client.query(
+        `
+                INSERT INTO user_websites(user_id, website_id)
+                VALUES($1, $2);
+            `,
+        [userWebsite.user_id, userWebsite.website_id]
+      );
+    }
+    console.log("Created User Websites table!");
+  } catch (error) {
+    throw error;
+  }
+};
+
+const buildDb = async () => {
+  try {
+    client.connect();
+    await dropTables();
+    await createTables();
+    await createInitialWebsiteContent();
+    await createInitialUsers();
+    await createInitialUserWebsites();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    client.end();
+  }
+};
+buildDb();
--- server/db/seedData.js
+++ server/db/seedData.js
@@ -1,39 +1,38 @@
 const websiteContent = [
   {
-    heading: "Melissa's Website",
+    main_heading: "Melissa's Website",
     subheading: "Made using Melissa's WebsiteBuilder",
-    headerPhoto:
+    main_photo:
       "https://www.myclickmagazine.com/wp-content/uploads/2019/02/Photographing_Minimalism_Composition_Negative_Space_by_Dana_Walton_16.jpg",
-    p1Heading: "Welcome.",
-    p1Body: "This is a test website. Thank you for visiting!",
-    midPhoto1:
+    p1_heading: "Welcome.",
+    p1_body: "This is a test website. Thank you for visiting!",
+    mid_photo1:
       "https://images.unsplash.com/photo-1487700160041-babef9c3cb55?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWluaW1hbGlzbXxlbnwwfHwwfHx8MA%3D%3D",
-    midphoto2:
+    mid_photo2:
       "https://images.unsplash.com/photo-1536882240095-0379873feb4e?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHp6fGVufDB8fDB8fHww",
-    p2Heading: "I hope that you enjoy my work.",
-    p2Body: "This is a really fun project.",
-    footerPhoto:
+    p2_heading: "I hope that you enjoy my work.",
+    p2_body: "This is a really fun project.",
+    footer_photo:
       "https://images.adsttc.com/media/images/6290/ac61/3e4b/31f4/d600/0001/large_jpg/155108.jpg?1653648474",
   },
 ];
 
 const users = [
   {
-    name: "Melissa",
+    first_name: "Melissa",
     username: "mel",
-    password: "mellyj123"
-  }
-]
+    password: "mellyj123",
+  },
+];
 
-const userContent = [
+const userWebsites = [
   {
-    user_id: 1, 
-    content_id: 1
-  }
-]
+    user_id: 1,
+    website_id: 1,
+  },
+];
 module.exports = {
   websiteContent,
-  users, 
+  users,
   userContent,
 };
-
